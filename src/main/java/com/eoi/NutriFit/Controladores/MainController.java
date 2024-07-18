4fc602b0bcf01090de3c73b8072be6c152e2d4eb
FranciscoConejoBarranco <conejofrancisco29@gmail.com>
2024-07-18 11:15:34 +0200

    arreglos de thymeleaf con html

--- src/main/java/com/eoi/NutriFit/Controladores/MainController.java
+++ src/main/java/com/eoi/NutriFit/Controladores/MainController.java
@@ -1,11 +1,54 @@
 package com.eoi.NutriFit.Controladores;
 
+import com.eoi.NutriFit.Entidades.Cliente;
+import com.eoi.NutriFit.Entidades.Usuario;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Map;
+
+@Controller
 public class MainController {
 
-    //@GetMapping("/usuarios")
-    //@PreAuthorize("hasAuthority('ADMIN')")
+    @GetMapping({"/", "/index"})
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping({"/blog"})
+    public String blog() {
+        return "blog";
+    }
+
+    @GetMapping({"/contact"})
+    public String contact() {
+        return "contact";
+    }
+    
+
+    @GetMapping({"/feature"})
+    public String feature() {
+        return "feature";
+    }
+
+    @GetMapping({"/about"})
+    public String about() {
+        return "about";
+    }
+
+    @GetMapping({"/404"})
+    public String notFound() {
+        return "404";
+    }
+
+    @GetMapping({"/testimonial"})
+    public String testimonial() {
+        return "testimonial";
+    }
 
 }
+
--- src/main/java/com/eoi/NutriFit/Controladores/ProductoController.java
+++ src/main/java/com/eoi/NutriFit/Controladores/ProductoController.java
@@ -2,7 +2,6 @@ package com.eoi.NutriFit.Controladores;
 
 import com.eoi.NutriFit.Entidades.Producto;
 import com.eoi.NutriFit.Servicios.ProductoService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
