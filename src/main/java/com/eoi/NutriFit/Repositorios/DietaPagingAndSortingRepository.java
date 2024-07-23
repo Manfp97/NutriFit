2f993f77e1a9218d14f93c2f67a6e92c0b4fbbb6
Manfp97 <manfp97@gmail.com>
2024-07-23 17:08:39 +0200

    Creacion de la rama "Dietas 0.0.2" con su paginación, dietalista.html y demás

---
+++ src/main/java/com/eoi/NutriFit/Repositorios/DietaPagingAndSortingRepository.java
@@ -0,0 +1,13 @@
+package com.eoi.NutriFit.Repositorios;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+import java.util.List;
+
+@NoRepositoryBean
+public interface DietaPagingAndSortingRepository<T, ID> extends JpaRepository<T, ID> {
+    List<T> findAll(Sort sort);
+    Page<T> findAll(Pageable pageable);
+}
